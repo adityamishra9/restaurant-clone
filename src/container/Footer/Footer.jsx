6155f4e6aa645954cfc9696ea7194775e57b03f4
Aditya Mishra <adityamishra5889@gmail.com>
2024-08-19 18:30:30 +0530

    bugfix:minor

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -33,13 +33,14 @@ const Footer = () => (
         <h1 className="app__footer-headtext">Working Hours</h1>
         <p className="p__opensans">Monday-Friday:</p>
         <p className="p__opensans">08:00 am - 12:00 am</p>
+
         <p className="p__opensans">Saturday-Sunday:</p>
         <p className="p__opensans">07:00 am - 11:00 pm</p>
       </div>
     </div>
 
     <div className="footer__copyright">
-      <p className="p__opensans">2021 Gericht. All Rights reserved.</p>
+      <p className="p__opensans">2024 Gericht. All Rights reserved.</p>
     </div>
 
   </div>
