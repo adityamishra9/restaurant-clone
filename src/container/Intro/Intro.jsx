f312ff4eda6cef82aeb3d7bd0ada48dade9308ec
Aditya Mishra <adityamishra5889@gmail.com>
2024-08-19 14:15:41 +0530

    feat:vid

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from "react";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+import "./Intro.css";
+import { meal } from "../../constants";
 
-import './Intro.css';
+const Intro = () => {
+  const [playVid, setPlayVid] = React.useState(false);
+  const vidRef = React.useRef();
+  const handleVid = () => {
+    setPlayVid((prevPlayVid) => !prevPlayVid);
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+    if (playVid) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  };
+
+  return (
+    <div className="app__vid">
+      <video
+        src={meal}
+        type="video/mp4"
+        loop
+        controls={false}
+        ref={vidRef}
+        muted
+      />
+      <div className="app__vid-overlay flex__center">
+        <div
+          className="app__vid-overlay_circle flex__center"
+          onClick={handleVid}
+        >
+          {playVid ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
